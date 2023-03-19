a80cc13c7a825ca38f18f184983203d5d6d3a645
diegopolanco69 <98666928+diegopolanco69@users.noreply.github.com>
2023-03-19 01:43:39 -0400

    Add files via upload

---
+++ ejercicio1/main.js
@@ -0,0 +1,15 @@
+//Crea variables para guardar los siguientes datos de una persona:
+
+//Nombre
+//Apellidos
+//Edad
+//Ciudad
+
+//Asignar valores a las variables creadas.
+
+let nombre = 'Diego';
+let apellido = 'Polanco';
+let edad = 28;
+let ciudad = 'Machala';
+
+console.log(nombre, apellido, edad, ciudad);
